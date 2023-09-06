ab306bfec3b1997857faacfc283113f224ce1c76
Vishal Gupta <117042853+Vishal12091995@users.noreply.github.com>
2023-09-07 03:19:57 +0530

    coding Ninjas Movie App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class App extends React.Component{
+  render(){
+    return (
+      <>
+      <h1>Movie App</h1>
+      <MovieCard />
+    </>
+    )
+  }
+}
+
+export default App;
